18ca3669183d03780b07f9056e53a11409eea8bf
Sarthak13121 <sarthak1312005@gmail.com>
2025-04-24 17:14:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,79 +1,77 @@
-const slidePage = document.querySelector(".slidepage");
-const nextBtn1 = document.querySelector(".nextbtn button");
-const nextBtn2 = document.querySelector(".next-1");
-const nextBtn3 = document.querySelector(".next-2");
-const prevBtn2 = document.querySelector(".prev-1");
-const prevBtn3 = document.querySelector(".prev-2");
-const prevBtn4 = document.querySelector(".prev-3");
-const submitBtn = document.querySelector(".submit");
+const slidePage = document.querySelector(".slide-page")
+const nextBtnFirst = document.querySelector(".firstNext")
+const prevBtnSec = document.querySelector(".prev-1")
+const nextBtnSec = document.querySelector(".next-1")
+const prevBtnThird = document.querySelector(".prev-2")
+const nextBtnThird = document.querySelector(".next-2")
+const prevBtnFourth = document.querySelector(".prev-3")
+const submitBtn = document.querySelector(".submit")
+const progressText = document.querySelectorAll(".step p")
+const progressCheck = document.querySelectorAll(".step .check")
+const bullet = document.querySelectorAll(".step .bullet")
 
-const bullets = document.querySelectorAll(".bullet");
-const progressTexts = document.querySelectorAll(".step p");
-const progressChecks = document.querySelectorAll(".step .check");
+let current = 1
 
-let current = 1;
+nextBtnFirst.addEventListener('click',function(event){
+    event.preventDefault()
+    slidePage.style.marginLeft = '-25%'
+    bullet[current - 1].classList.add('active')
+    progressCheck[current - 1].classList.add('active')
+    progressText[current - 1].classList.add('active')
+    current +=1
+})
 
-nextBtn1.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "-25%";
-    updateProgress(current);
-    current++;
-});
+nextBtnSec.addEventListener('click',function(event){
+    event.preventDefault();
+    slidePage.style.marginLeft = '-50%'
+    bullet[current - 1].classList.add('active')
+    progressCheck[current - 1].classList.add('active')
+    progressText[current - 1].classList.add('active')
+    current +=1
+})
 
-nextBtn2.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "-50%";
-    updateProgress(current);
-    current++;
-});
+nextBtnThird.addEventListener('click',function(event){
+    event.preventDefault()
+    slidePage.style.marginLeft = '-75%'
+    bullet[current - 1].classList.add('active')
+    progressCheck[current - 1].classList.add('active')
+    progressText[current - 1].classList.add('active')
+    current +=1
+})
 
-nextBtn3.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "-75%";
-    updateProgress(current);
-    current++;
-});
+submitBtn.addEventListener('click',function(){
+    bullet[current - 1].classList.add('active')
+    progressCheck[current - 1].classList.add('active')
+    progressText[current - 1].classList.add('active')
+    current +=1
+    setTimeout(function(){
+        alert("Your Form Successfully Signed up")
+        //location.reload()
+    },800)
+})
 
-submitBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    updateProgress(current);
-    console.log("submit button:",submitBtn)
-    current++;
-    setTimeout(() => {
-        alert("You're successfully signed up");
-        location.reload();
-    }, 800);
-});
+prevBtnSec.addEventListener('click',function(event){
+    event.preventDefault()
+    slidePage.style.marginLeft='0%'
+    bullet[current -2].classList.remove('active')
+    progressCheck[current -2].classList.remove('active')
+    progressText[current - 2].classList.remove('active')
+    current-=1
+})
 
-prevBtn2.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "0%";
-    rollbackProgress(current - 2);
-    current--;
-});
-
-prevBtn3.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "-25%";
-    rollbackProgress(current - 2);
-    current--;
-});
-
-prevBtn4.addEventListener("click", (e) => {
-    e.preventDefault();
-    slidePage.style.marginLeft = "-50%";
-    rollbackProgress(current - 2);
-    current--;
-});
-
-function updateProgress(index) {
-    bullets[index].classList.add("active");
-    progressTexts[index].classList.add("active");
-    progressChecks[index].classList.add("active");
-}
-
-function rollbackProgress(index) {
-    bullets[index].classList.remove("active");
-    progressTexts[index].classList.remove("active");
-    progressChecks[index].classList.remove("active");
-}
+prevBtnThird.addEventListener('click',function(event){
+    event.preventDefault()
+    slidePage.style.marginLeft='-25%'
+    bullet[current -2].classList.remove('active')
+    progressCheck[current -2].classList.remove('active')
+    progressText[current - 2].classList.remove('active')
+    current-=1
+})
+prevBtnFourth.addEventListener('click',function(event){
+    event.preventDefault()
+    slidePage.style.marginLeft='-50%'
+    bullet[current -2].classList.remove('active')
+    progressCheck[current -2].classList.remove('active')
+    progressText[current - 2].classList.remove('active')
+    current-=1
+})
